17c43bef2f4780c0feda1900bd1e04c4ae06e6b5
Clement <clementclementson@gmail.com>
2023-10-23 21:39:12 +0400

    Initial commit

---
+++ src/main/java/com/app/hub/uaa/AppHubUaaApplication.java
@@ -0,0 +1,13 @@
+package com.app.hub.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHubUaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHubUaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/hub/uaa/AppHubUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hub.uaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHubUaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
