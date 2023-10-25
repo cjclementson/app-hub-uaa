ff16f173415a8f7fd949f67056f19eb6dbeee2a4
Clement <clementclementson@gmail.com>
2023-10-25 17:57:46 +0400

    Updated filter chain to use custom frontend for login

--- src/main/java/com/app/hub/uaa/security/SecurityConfiguration.java
+++ src/main/java/com/app/hub/uaa/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.app.hub.uaa.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.text.Normalizer.Form;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -35,10 +37,16 @@ public class SecurityConfiguration {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
 		return http.csrf(csrf -> csrf.disable())
+			//.cors(cors -> cors.disable())
 			.authorizeHttpRequests((requests) -> requests
 			.requestMatchers("/api/v1/user/**").authenticated()
+			.requestMatchers("/login").permitAll()
 			.requestMatchers("/api/v1/auth/**").permitAll())
-			.formLogin(withDefaults())
+			.formLogin(form -> 
+				form.loginPage("http://localhost:5500/login-html-css/index.html")
+					.loginProcessingUrl("/login")
+					.defaultSuccessUrl("/api/v1/user/2").permitAll())
+			//.formLogin(withDefaults())
 			.httpBasic(withDefaults())
 			.build();
 	}
